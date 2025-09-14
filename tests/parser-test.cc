f4721f95566a0f9bc4d653765949f4abd488d32a
leafcabral <rafaelcabral4100@gmail.com>
2025-09-13 22:53:29 -0300

    Created (bad) testcase for tokenize()

--- tests/parser-test.cc
+++ tests/parser-test.cc
@@ -1,10 +1,40 @@
 #include "parser.h"
 
-#include <print>
+#include <string>
+#include <vector>
 #include <cassert>
 
+struct TestCase {
+	std::string input;
+	std::string output;
+
+	TestCase(std::string input, std::string output) {
+		this.input = input;
+		this.output = output;
+	}
+
+	std::string comparisonString(std::string realOutput) {
+		return '\'' + this.output, + "\' == \'" + realOutput + '\'';
+	}
+}
+
 int main(void) {
-	std::println("Test :o");
-	assert(1 == 1);
+	std::vector<TestCase> t_tokenize = {
+		TestCase("1 * 2 + 3", "1 * 2 + 3"),
+		TestCase("1 + 2 * 3", "1 + 2 * 3"),
+		TestCase("1 * (2 + 3)", "1 * (2 + 3)"),
+		TestCase("1 - 2 + 3", "1 - 2 + 3"),
+		TestCase("1 * 2 ^ 3 + 4", "1 * 2 ^ 3 + 4"),
+		TestCase("1 * (2 + 3 * 4) + 5", "1 * (2 + 3 * 4) + 5")
+	};
+
+	for (auto test : t_tokenize) {
+		std::string realOutput = Parser::toString(Parser::tokenize(test.input));
+
+		std::cout << test.comparisonString(realOutput);
+		assert(test.output == realOutput)
+		std::cout << "Pass.\n";
+	}
+	
 	return 0;
 }
